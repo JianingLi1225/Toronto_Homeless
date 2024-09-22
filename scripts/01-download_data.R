ed6155be680cc7889577140f6a137548498f1184
JianingLi1225 <lijianing.li@mail.utoronto.ca>
2024-09-22 16:18:05 -0400

    描述你的更改，例如：删除不再需要的模型文件，更新数据清理脚本

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,6 +1,6 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
+# Author: Jianing Li
 # Date: 11 February 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
 # License: MIT
@@ -11,24 +11,21 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-packages<-list_packages(500)
+packages <- list_packages(500)
 packages
 
-shelter_system_packages<-search_packages("Toronto Shelter System Flow")
+shelter_system_packages <- search_packages("Toronto Shelter System Flow")
 shelter_system_packages
 
-shelter_system_resources<-shelter_system_packages %>%
+shelter_system_resources <- shelter_system_packages %>%
   list_package_resources()
 shelter_system_resources
 
 #### Download data ####
-shelter_system_statistics<-shelter_system_resources[2,] %>%
+shelter_system_statistics <- shelter_system_resources[2, ] %>%
   get_resource()
 shelter_system_statistics
 
-
 #### Save data ####
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(shelter_system_statistics, "/Users/liz/Downloads/starter_folder-main/data/raw_data/shelter_raw_data.csv") 
-
-         
+write_csv(shelter_system_statistics, "/Users/liz/Downloads/starter_folder-main/data/raw_data/shelter_raw_data.csv")
